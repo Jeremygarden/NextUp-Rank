e4a563aeb82512630aa62788bac8f720885f90d0
AC <mingxlee23@gmail.com>
2026-03-20 13:57:40 +0000

    postcss.config.js is REQUIRED, module path fixed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./ui/**/*.{js,ts,jsx,tsx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
